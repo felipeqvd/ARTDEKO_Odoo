27af354a8da0703965bdb03651c37798b0144fd6
DESKTOP-L7N7GSP\Felipe Quevedo <felipeqvd@hotmail.com>
2018-08-21 18:55:23 -0500

    Depuración por pasos1

--- artdeko/models/product_product_artdeko.py
+++ artdeko/models/product_product_artdeko.py
@@ -6,13 +6,4 @@ from odoo import api, fields, models
 class ProductProductArtdeko(models.Model):
     _inherit = 'product.product'
 
-    @api.multi
-    def _sales_product_attributes(self):
-        r = {}       
-        domain = [
-            ('state', 'in', ['sale', 'done']),
-            ('product_id', 'in', self.ids),
-        ]
-        for group in self.env['sale.report'].read_group(domain, ['product_id', 'product_uom_qty', 'l10n_mx_edi_code_sat_id'], ['product_id']):
-            r[group['product_id'][0]] = group['l10n_mx_edi_code_sat_id']        
-        return r
+    
