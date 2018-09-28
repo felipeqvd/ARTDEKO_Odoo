a2d8f06f3a2487e3cf02b40f2189617d1b10e0b8
DESKTOP-L7N7GSP\Felipe Quevedo <felipeqvd@hotmail.com>
2018-09-27 23:45:40 -0500

    150

--- artdeko/models/purchase.py
+++ artdeko/models/purchase.py
@@ -14,7 +14,7 @@ class PurchaseOrder(models.Model):
         :rtype: str
         """
         self.ensure_one()
-        currency = self.currency_id.name.upper()
+        currency = self.order.currency_id.name.upper()
         # M.N. = Moneda Nacional (National Currency)
         # M.E. = Moneda Extranjera (Foreign Currency)
         currency_type = 'M.N' if currency == 'MXN' else 'M.E.'
@@ -22,7 +22,7 @@ class PurchaseOrder(models.Model):
         amount_i, amount_d = divmod(self.amount_total, 1)
         amount_d = round(amount_d, 2)
         amount_d = int(round(amount_d * 100, 2))
-        words = self.currency_id.with_context(lang=self.partner_id.lang or 'es_ES').amount_to_text(amount_i).upper()
+        words = self.order.currency_id.with_context(lang=self.partner_id.lang or 'es_ES').amount_to_text(amount_i).upper()
         purchase_order_words = '%(words)s %(amount_d)02d/100 %(curr_t)s' % dict(
             words=words, amount_d=amount_d, curr_t=currency_type)
         return purchase_order_words
