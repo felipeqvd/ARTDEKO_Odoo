26524a6a7d363c4eceb4569a92a61f3f37ec6dba
DESKTOP-L7N7GSP\Felipe Quevedo <felipeqvd@hotmail.com>
2018-09-28 10:27:49 -0500

    158

--- artdeko/models/purchase.py
+++ artdeko/models/purchase.py
@@ -7,25 +7,6 @@ class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     @api.multi
-    def purchase_amount_to_text(self):
-        """Method to transform a float amount to text words
-        E.g. 100 - ONE HUNDRED
-        :returns: Amount transformed to words mexican format for invoices
-        :rtype: str
-        """
-        #self.ensure_one()
-        #currency = self.order.currency_id.name.upper()
-        # M.N. = Moneda Nacional (National Currency)
-        # M.E. = Moneda Extranjera (Foreign Currency)
-        #currency_type = 'M.N' if currency == 'MXN' else 'M.E.'
-        #currency_type = 'M.N'
-        # Split integer and decimal part
-        #amount_i, amount_d = divmod(self.amount_total, 1)
-        #amount_d = round(amount_d, 2)
-        #amount_d = int(round(amount_d * 100, 2))
-        #words = self.order.currency_id.with_context(lang=self.partner_id.lang or 'es_ES').amount_to_text(amount_i).upper()
-        #words = amount_to_text(amount_i).upper()
-        #purchase_order_words = '%(words)s %(amount_d)02d/100 %(curr_t)s' % dict(
-        #    words=words, amount_d=amount_d, curr_t=currency_type)
+    def purchase_amount_to_text(self):        
         purchase_order_words = 'Valor en letras'
         return purchase_order_words
