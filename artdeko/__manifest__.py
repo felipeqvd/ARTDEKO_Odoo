7d80fa2d1d5881e696631777680aceedc6512181
DESKTOP-L7N7GSP\Felipe Quevedo <felipeqvd@hotmail.com>
2018-10-08 00:10:20 -0500

    Ajuste amount to text

--- artdeko/__manifest__.py
+++ artdeko/__manifest__.py
@@ -22,7 +22,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'product', 'sale'],
+    'depends': ['base', 'product', 'sale','purchase'],
 
     # always loaded
     'data': [
