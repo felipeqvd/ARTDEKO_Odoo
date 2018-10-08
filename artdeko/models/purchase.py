1b85e98a5000ddf706eca7d486f2b0ec2f195f83
DESKTOP-L7N7GSP\Felipe Quevedo <felipeqvd@hotmail.com>
2018-10-07 22:56:05 -0500

    Pruebas modificar modelo purchase

--- artdeko/models/purchase.py
+++ artdeko/models/purchase.py
@@ -2,11 +2,3 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
-
-class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
-
-    @api.multi
-    def purchase_amount_to_text(self):        
-        purchase_order_words = 'Valor en letras'
-        return purchase_order_words
