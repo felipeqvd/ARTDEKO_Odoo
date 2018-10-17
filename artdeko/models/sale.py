08c1387e026661bbfbd11b87714bc8c9595f4032
DESKTOP-L7N7GSP\Felipe Quevedo <felipeqvd@hotmail.com>
2018-10-17 10:15:38 -0500

    235

--- artdeko/models/sale.py
+++ artdeko/models/sale.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+from datetime import datetime
+from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from odoo.tools.float_utils import float_is_zero, float_compare
+from odoo.tools.misc import formatLang
+from odoo.addons import decimal_precision as dp
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
