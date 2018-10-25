1d758b3efa6a6a41f03936e66cbb17bb0e6ea2dc
DESKTOP-L7N7GSP\Felipe Quevedo <felipeqvd@hotmail.com>
2018-10-24 19:43:36 -0500

    271

--- artdeko/models/sale.py
+++ artdeko/models/sale.py
@@ -72,7 +72,7 @@ class SaleOrder(models.Model):
             'views': [[False, 'form']],
             'target': 'new',
             'context': {
-                'default_activity_type_id': '6',
+                'default_activity_type_id': 6,
                 'default_res_id': self.id,
                 'default_res_model': 'sale.order',
                 'default_summary': 'Solicitud de Orden de Compra',
